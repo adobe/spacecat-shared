db5a14af6d09333cea10af8624e07454bbeac838
Dominique Jäggi <djaeggi@adobe.com>
2024-01-22 11:40:28 +0100

    fix: github semantic plugin config

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -11,7 +11,7 @@ module.exports = {
       "assets": ["package.json", "CHANGELOG.md"],
       "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
     }],
-    ["@semantic-release/github", {}]
+    ["@semantic-release/github", {}],
   ],
   branches: ['main'],
 };

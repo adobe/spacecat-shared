fbac2ef9eba8c9bd3dec51d83ab880f7e35dab82
Bhanu Dash <satdash@adobe.com>
2024-01-24 15:24:40 -0800

    Revertback .releaserc.cjs

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -1,22 +1,17 @@
 module.exports = {
+  extends: "semantic-release-monorepo",
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     ["@semantic-release/changelog", {
-      changelogFile: "CHANGELOG.md",
+      "changelogFile": "CHANGELOG.md",
     }],
-    ['@semantic-release/npm', {
-      npmPublish: false,
-    }],
+    "@semantic-release/npm",
     ["@semantic-release/git", {
-      assets: ['package.json', 'package-lock.json', 'CHANGELOG.md', 'docs/index.html'],
-      message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      "assets": ["package.json", "CHANGELOG.md"],
+      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
     }],
-    ['@semantic-release/exec', {
-      prepareCmd: 'npm run deploy && npm run test-postdeploy',
-      publishCmd: 'npm run deploy-routes'
-    }],
-    ["@semantic-release/github", {}]
+    ["@semantic-release/github", {}],
   ],
   branches: ['main'],
 };

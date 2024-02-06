7fbe16bd207cfd54b29898d01659c389b84efdf1
Damian Zehnder <zehnder@adobe.com>
2024-02-06 11:55:44 +0100

    feat: introduce AWS S3 wrapper

---
+++ packages/spacecat-shared-utils/src/s3.js
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2024 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+import { S3Client } from '@aws-sdk/client-s3';
+
+export function s3Bucket(fn) {
+  return async (req, context) => {
+    if (!context.s3) {
+      const {
+        AWS_REGION: region,
+      } = context.env;
+
+      context.s3 = new S3Client({ region });
+    }
+    return fn(req, context);
+  };
+}

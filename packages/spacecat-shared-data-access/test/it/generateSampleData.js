5ed489dc84cb594689092e1f0019dd83d3647039
Dominique Jäggi <1872195+solaris007@users.noreply.github.com>
2024-06-28 14:57:42 +0200

    feat: add helix config to site/candidates (#277)
    Adding `hlxConfig` attribute to both `sites` and `site-candidates` in
    preparation for site-detection enriching this information.

--- packages/spacecat-shared-data-access/test/it/generateSampleData.js
+++ packages/spacecat-shared-data-access/test/it/generateSampleData.js
@@ -276,6 +276,25 @@ export default async function generateSampleData(
       GSI1PK: config.pkAllSites,
       createdAt: nowIso,
       updatedAt: nowIso,
+      hlxConfig: {
+        cdnProdHost: 'www.example.com',
+        code: {
+          owner: 'some-owner',
+          repo: 'some-repo',
+          source: {
+            type: 'github',
+            url: 'https://github.com/some-owner/some-repo',
+          },
+        },
+        content: {
+          contentBusId: '1234',
+          source: {
+            type: 'onedrive',
+            url: 'https://some-owner.sharepoint.com/:f:/r/sites/SomeFolder/Shared%20Documents/some-site/www',
+          },
+        },
+        hlxVersion: 5,
+      },
       config: {
         slack: {
           workspace: `${i}-workspace`,
@@ -313,6 +332,30 @@ export default async function generateSampleData(
     siteCandidates.push({
       baseURL: `https://example${i}.com`,
       status: SITE_CANDIDATE_STATUS.PENDING,
+      hlxConfig: {
+        rso: {
+          owner: 'some-owner',
+          site: `some-site${i}`,
+          ref: 'main',
+        },
+        cdnProdHost: `www.example${i}.com`,
+        code: {
+          owner: 'some-owner',
+          repo: 'some-repo',
+          source: {
+            type: 'github',
+            url: `https://github.com/some-owner/some-repo${i}`,
+          },
+        },
+        content: {
+          contentBusId: '1234',
+          source: {
+            type: 'onedrive',
+            url: `https://some-owner.sharepoint.com/:f:/r/sites/SomeFolder/Shared%20Documents/some-site${i}/www`,
+          },
+        },
+        hlxVersion: 5,
+      },
     });
   }
 

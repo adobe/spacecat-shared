94d639641a9b9967754d32f7c9434180b2c1c55a
alinarublea <alina.rublea@gmail.com>
2024-07-16 13:35:04 +0200

    fix: add migration script

--- packages/spacecat-shared-data-access/migration.sh
+++ packages/spacecat-shared-data-access/migration.sh
@@ -57,6 +57,18 @@ EOF
         fi
     done | jq -s 'add')
 
+    if [ -n "$SLACK_WORKSPACE" ] && [ -n "$SLACK_CHANNEL" ]; then
+        SLACK_CONFIG=$(cat <<EOF
+"slack": {"M": {"workspace": {"S": "$SLACK_WORKSPACE"}, "channel": {"S": "$SLACK_CHANNEL"}}}
+EOF
+)
+    else
+        SLACK_CONFIG=$(cat <<EOF
+"slack": {"M": {}}
+EOF
+)
+    fi
+
     MIGRATED_SITE=$(cat <<EOF
 {
     "id": {"S": "$SITE_ID"},
@@ -71,7 +83,7 @@ EOF
     "updatedAt": {"S": "$UPDATED_AT"},
     "config": {
         "M": {
-            "slack": {"M": {"workspace": {"S": "$SLACK_WORKSPACE"}, "channel": {"S": "$SLACK_CHANNEL"}}},
+            $SLACK_CONFIG,
             "handlers": {"M": $HANDLERS}
         }
     }
@@ -80,52 +92,4 @@ EOF
 )
 
     # Insert migrated site data into the site table
-    $AWS_CMD put-item --table-name $SITE_TABLE --item "$MIGRATED_SITE" --region $REGION
-    if [ $? -ne 0 ]; then
-        echo "Failed to migrate site with ID $SITE_ID"
-    fi
-done
-
-# Migrate each organization
-echo "$ORGANIZATIONS" | jq -c '.Items[]' | while read -r org; do
-    ORG_ID=$(echo $org | jq -r '.id.S')
-    IMS_ORG_ID=$(echo $org | jq -r '.imsOrgId.S')
-    NAME=$(echo $org | jq -r '.name.S')
-    GSI1PK=$(echo $org | jq -r '.GSI1PK.S')
-    CREATED_AT=$(echo $org | jq -r '.createdAt.S')
-    UPDATED_AT=$(echo $org | jq -r '.updatedAt.S')
-    SLACK_WORKSPACE=$(echo $org | jq -r '.config.M.slack.M.workspace.S // empty')
-    SLACK_CHANNEL=$(echo $org | jq -r '.config.M.slack.M.channel.S // empty')
-    ALERT_TYPE=$(echo $org | jq -r '.config.M.alerts.L[0].M.type.S // empty')
-    ALERT_COUNTRY=$(echo $org | jq -r '.config.M.alerts.L[1].M.country.S // empty')
-
-    MIGRATED_ORG=$(cat <<EOF
-{
-    "id": {"S": "$ORG_ID"},
-    "imsOrgId": {"S": "$IMS_ORG_ID"},
-    "name": {"S": "$NAME"},
-    "GSI1PK": {"S": "$GSI1PK"},
-    "createdAt": {"S": "$CREATED_AT"},
-    "updatedAt": {"S": "$UPDATED_AT"},
-    "config": {
-        "M": {
-            "slack": {"M": {"workspace": {"S": "$SLACK_WORKSPACE"}, "channel": {"S": "$SLACK_CHANNEL"}}},
-            "handlers": {
-                "M": {
-                    "$ALERT_TYPE": {"M": {"enabledByDefault": {"BOOL": false}, "country": {"S": "$ALERT_COUNTRY"}}}
-                }
-            }
-        }
-    }
-}
-EOF
-)
-
-    # Insert migrated organization data into the organization table
-    $AWS_CMD put-item --table-name $ORGANIZATION_TABLE --item "$MIGRATED_ORG" --region $REGION
-    if [ $? -ne 0 ]; then
-        echo "Failed to migrate organization with ID $ORG_ID"
-    fi
-done
-
-echo "Migration completed successfully."
+    $AWS_CMD put-item --table-name $

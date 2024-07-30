9c91857e74f5fd3540c232fb07aa790c1a1397cf
Dominique Jäggi <djaeggi@adobe.com>
2024-07-30 13:02:20 +0200

    feat: move auth wrappers from api-service

--- packages/spacecat-shared-http-utils/src/auth/handlers/config/ims.js
+++ packages/spacecat-shared-http-utils/src/auth/handlers/config/ims.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 Adobe. All rights reserved.
+ * Copyright 2024 Adobe. All rights reserved.
  * This file is licensed to you under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License. You may obtain a copy
  * of the License at http://www.apache.org/licenses/LICENSE-2.0
@@ -9,36 +9,15 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-
-module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
+export default {
+  name: 'ims-na1',
+  discoveryUrl: 'https://ims-na1.adobelogin.com/ims/.well-known/openid-configuration',
+  discovery: {
+    issuer: 'https://ims-na1.adobelogin.com',
+    authorization_endpoint: 'https://ims-na1.adobelogin.com/ims/authorize/v2',
+    token_endpoint: 'https://ims-na1.adobelogin.com/ims/token/v3',
+    userinfo_endpoint: 'https://ims-na1.adobelogin.com/ims/userinfo/v2',
+    revocation_endpoint: 'https://ims-na1.adobelogin.com/ims/revoke',
+    jwks_uri: 'https://ims-na1.adobelogin.com/ims/keys',
   },
-  extends: [
-    '@adobe/helix',
-    'plugin:@typescript-eslint/recommended',
-  ],
-  plugins: [
-    'import',
-    '@typescript-eslint',
-  ],
-  overrides: [
-    {
-      files: ['*.ts'],
-      rules: {},
-    },
-    {
-      files: ['*.js', '*.cjs'],
-      rules: {},
-    },
-    {
-      files: ['*.test.js'],
-      rules: {
-        'no-unused-expressions': 'off',
-      }
-    }
-  ],
 };

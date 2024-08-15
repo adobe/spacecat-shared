a89851709e588d3877615620aaf047a8d3dd27d6
Sweta Barman <swbarman@adobe.com>
2024-08-15 12:11:17 -0400

    fix: refactor hashedApiKey

--- packages/spacecat-shared-http-utils/src/auth/handlers/scoped-api-key.js
+++ packages/spacecat-shared-http-utils/src/auth/handlers/scoped-api-key.js
@@ -36,8 +36,8 @@ export default class ScopedApiKeyHandler extends AbstractHandler {
     }
 
     // Keys are stored by their hash, so we need to hash the key to look it up
-    const hashedKey = hashWithSHA256(apiKeyFromHeader);
-    const apiKeyEntity = await dataAccess.getApiKeyByHashedKey(hashedKey);
+    const hashedApiKey = hashWithSHA256(apiKeyFromHeader);
+    const apiKeyEntity = await dataAccess.getApiKeyByHashedApiKey(hashedApiKey);
 
     if (!apiKeyEntity) {
       this.log(`No API key entity found in the data layer for the provided API key: ${apiKeyFromHeader}`, 'error');

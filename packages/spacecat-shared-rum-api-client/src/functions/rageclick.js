34705681ff593c6d8de96ef41561bbfd3dfc4bdc
Ravi Kiran Papani <rpapani@adobe.com>
2024-08-15 16:02:03 -0700

    fix: filtering based on the commerce selectors to avoid noise and refactoring

--- packages/spacecat-shared-rum-api-client/src/functions/rageclick.js
+++ packages/spacecat-shared-rum-api-client/src/functions/rageclick.js
@@ -19,13 +19,27 @@
 /* c8 ignore start */
 const DEFAULT_RAGE_CLICK_THRESHOLD = 10;
 const DEFAULT_RAGECLICK_PERCENT_THRESHOLD = 5;
-const DEFAULT_PAGEVIEW_THRESHOLD = 5000;
+const DEFAULT_RAGECLICK_PAGEVIEW_THRESHOLD = 5000;
 
-function getRageClickSelectors(events) {
+const COMMERCE_SELECTORS_IGNORE_LIST = [
+  '.product-list-page',
+  '.product-details-verb',
+  '.product-details',
+];
+
+const OPPORTUNITY_TYPE = 'rageclick';
+const OPPORTUNITY_DESCRIPTION = 'The percentage of users who click on the same element lot of times in a short period of time.';
+
+/**
+ * Returns the selectors that have more than DEFAULT_RAGE_CLICK_THRESHOLD clicks from the events
+ * @param {*} events
+ * @returns
+ */
+function getRageClickSelectors(events, threshold, selectorsIgnoreList = []) {
   const clickSelectors = {};
   for (const event of events) {
-    if (event.checkpoint === 'click') {
-      const { source } = event;
+    const { source, checkpoint } = event;
+    if (checkpoint === 'click' && !selectorsIgnoreList.includes(source)) {
       if (!clickSelectors[source]) {
         clickSelectors[source] = 0;
       }
@@ -33,16 +47,90 @@ function getRageClickSelectors(events) {
     }
   }
   for (const selector of Object.keys(clickSelectors)) {
-    if (clickSelectors[selector] < DEFAULT_RAGE_CLICK_THRESHOLD) {
+    if (clickSelectors[selector] < threshold) {
       delete clickSelectors[selector];
     }
   }
   return clickSelectors;
 }
 
+function filterRageClickInstancesByThreshold(
+  rageClickInstances,
+  pageData,
+  rageClickPercentThreshold,
+  rageClickPageviewThreshold,
+) {
+  for (const url of Object.keys(rageClickInstances)) {
+    if (pageData[url].pageViews < rageClickPageviewThreshold) {
+      // eslint-disable-next-line no-param-reassign
+      delete rageClickInstances[url];
+    } else {
+      for (const selector of Object.keys(rageClickInstances[url])) {
+        const rageClickPercentage = (
+          rageClickInstances[url][selector].samples / pageData[url].samples) * 100;
+        if (rageClickPercentage < rageClickPercentThreshold) {
+          // eslint-disable-next-line no-param-reassign
+          delete rageClickInstances[url][selector];
+        } else {
+          // eslint-disable-next-line no-param-reassign
+          rageClickInstances[url][selector].percentage = rageClickPercentage;
+        }
+      }
+      if (Object.keys(rageClickInstances[url]).length === 0) {
+        // eslint-disable-next-line no-param-reassign
+        delete rageClickInstances[url];
+      } else {
+        // eslint-disable-next-line no-param-reassign
+        rageClickInstances[url].pageViews = pageData[url].pageViews;
+        // eslint-disable-next-line no-param-reassign
+        rageClickInstances[url].samples = pageData[url].samples;
+      }
+    }
+  }
+}
+
+function getRageClickOpporunities(rageClickInstances) {
+  const opportunities = [];
+  for (const url of Object.keys(rageClickInstances)) {
+    const opportunity = {
+      type: OPPORTUNITY_TYPE,
+      page: url,
+      screenshot: '',
+      trackedPageKPIName: OPPORTUNITY_DESCRIPTION,
+      trackedPageKPIValue: '',
+      pageViews: rageClickInstances[url].pageViews,
+      samples: rageClickInstances[url].samples,
+      metrics: [],
+    };
+    for (const selector of Object.keys(rageClickInstances[url])) {
+      if (typeof rageClickInstances[url][selector] === 'object') {
+        opportunity.metrics.push({
+          type: 'click',
+          selector,
+          value: rageClickInstances[url][selector].value,
+          samples: rageClickInstances[url][selector].samples,
+          percentage: rageClickInstances[url][selector].percentage,
+        });
+      }
+    }
+    const avgRageClickPercentage = opportunity.metrics.reduce(
+      (acc, metric) => acc + metric.percentage,
+      0,
+    ) / opportunity.metrics.length;
+    opportunity.trackedPageKPIValue = avgRageClickPercentage;
+    opportunities.push(opportunity);
+  }
+  return opportunities;
+}
+
 function handler(bundles) {
   const rageClickInstances = {};
   const pageData = {};
+  const rageClickThreshold = process.env.RAGE_CLICK_THRESHOLD || DEFAULT_RAGE_CLICK_THRESHOLD;
+  const rageClickPercentThreshold = process.env.RAGE_CLICK_PERCENT_THRESHOLD
+  || DEFAULT_RAGECLICK_PERCENT_THRESHOLD;
+  const rageClickPageviewThreshold = process.env.RAGE_CLICK_PAGEVIEW_THRESHOLD
+  || DEFAULT_RAGECLICK_PAGEVIEW_THRESHOLD;
   for (const bundle of bundles) {
     const { url, weight } = bundle;
     if (!pageData[url]) {
@@ -54,7 +142,11 @@ function handler(bundles) {
       pageData[url].pageViews += weight;
       pageData[url].samples += 1;
     }
-    const rageClickSelectors = getRageClickSelectors(bundle.events);
+    const rageClickSelectors = getRageClickSelectors(
+      bundle.events,
+      rageClickThreshold,
+      COMMERCE_SELECTORS_IGNORE_LIST,
+    );
     if (Object.keys(rageClickSelectors).length > 0) {
       if (!rageClickInstances[url]) {
         rageClickInstances[url] = {};
@@ -71,28 +163,13 @@ function handler(bundles) {
       }
     }
   }
-  for (const url of Object.keys(rageClickInstances)) {
-    if (pageData[url].pageViews < DEFAULT_PAGEVIEW_THRESHOLD) {
-      delete rageClickInstances[url];
-    } else {
-      for (const selector of Object.keys(rageClickInstances[url])) {
-        const rageClickPercentage = (
-          rageClickInstances[url][selector].samples / pageData[url].samples) * 100;
-        if (rageClickPercentage < DEFAULT_RAGECLICK_PERCENT_THRESHOLD) {
-          delete rageClickInstances[url][selector];
-        } else {
-          rageClickInstances[url][selector].percentage = rageClickPercentage;
-        }
-      }
-      if (Object.keys(rageClickInstances[url]).length === 0) {
-        delete rageClickInstances[url];
-      } else {
-        rageClickInstances[url].pageViews = pageData[url].pageViews;
-        rageClickInstances[url].samples = pageData[url].samples;
-      }
-    }
-  }
-  return rageClickInstances;
+  filterRageClickInstancesByThreshold(
+    rageClickInstances,
+    pageData,
+    rageClickPercentThreshold,
+    rageClickPageviewThreshold,
+  );
+  return getRageClickOpporunities(rageClickInstances);
 }
 
 export default {

cdc7e3c5dce8e59e778a4bd3a127751c44409c65
Karl Pauls <kpauls@adobe.com>
2024-09-19 10:26:01 +0200

    Cleanup a bit

--- packages/spacecat-shared-rum-api-client/src/functions/experiment3.js
+++ packages/spacecat-shared-rum-api-client/src/functions/experiment3.js
@@ -17,55 +17,51 @@ function handler(bundles) {
 
   dataChunks.load(bundles);
 
-  dataChunks.addFacet('experiments', (bundle) => [...new Set(bundle.events.filter((e) => e.checkpoint === 'experiment')
-    .flatMap((e) => bundle.events.filter((c) => ['click', 'formsubmit', 'convert'].includes(c.checkpoint) && c.source).flatMap((c) => [`${bundle.url}\u3343${e.source}\u3343${e.target}\u3343${c.checkpoint}\u3343${c.source}`, `${bundle.url}\u3343${e.source}\u3343${e.target}\u3343${c.checkpoint}\u3343*`]).concat([`${bundle.url}\u3343${e.source}\u3343${e.target}`, `${bundle.url}\u3343${e.source}`])))]);
+  dataChunks.addFacet('experiments', (bundle) => bundle.events.filter((e) => e.checkpoint === 'experiment').map((e) => `${bundle.url}\u3343${e.source}`));
+  dataChunks.addFacet('variants', (bundle) => bundle.events.filter((e) => e.checkpoint === 'experiment').map((e) => `${bundle.url}\u3343${e.source}\u3343${e.target}`));
+  dataChunks.addFacet('checkpoints', (bundle) => [...new Set(bundle.events.filter((e) => e.checkpoint === 'experiment')
+    .flatMap((e) => bundle.events.filter((c) => ['click', 'formsubmit', 'convert'].includes(c.checkpoint) && c.source).flatMap((c) => [`${bundle.url}\u3343${e.source}\u3343${e.target}\u3343${c.checkpoint}\u3343${c.source}`, `${bundle.url}\u3343${e.source}\u3343${e.target}\u3343${c.checkpoint}\u3343*`])))]);
 
   dataChunks.addSeries('experimenttime', (bundle) => new Date(bundle.time).getTime());
 
   dataChunks.addSeries('views', (bundle) => bundle.weight);
   dataChunks.addSeries('interaction', (bundle) => (bundle.events.find((e) => e.checkpoint === 'click' || e.checkpoint === 'formsubmit' || e.checkpoint === 'convert') ? bundle.weight : undefined));
 
-  const { experiments } = dataChunks.facets;
+  const { experiments, variants, checkpoints } = dataChunks.facets;
 
   const result = {};
   experiments.forEach((uev) => {
-    const [url, experiment, variant, checkpoint, source] = uev.value.split('\u3343');
+    const [url, experiment] = uev.value.split('\u3343');
     if (!result[url]) result[url] = [];
-    let eIdx = result[url].findIndex((e) => e.experiment === experiment);
-    if (eIdx === -1) {
-      result[url].push({
-        experiment,
-        variants: [],
-      });
-      eIdx = result[url].length - 1;
-    }
-    if (variant) {
-      let vIdx = result[url][eIdx].variants.findIndex((v) => v.name === variant);
-      if (vIdx === -1) {
-        result[url][eIdx].variants.push({
-          name: variant,
-          click: {},
-          formsubmit: {},
-          convert: {},
-        });
-        vIdx = result[url][eIdx].variants.length - 1;
-      }
-      if (checkpoint && source) {
-        result[url][eIdx].variants[vIdx][checkpoint][source] = {
-          value: uev.metrics.views.sum,
-          samples: uev.metrics.views.count,
-        };
-      } else {
-        result[url][eIdx].variants[vIdx].interactionsCount = uev.metrics.interaction.sum;
-        result[url][eIdx].variants[vIdx].samples = uev.metrics.views.count;
-        result[url][eIdx].variants[vIdx].views = uev.metrics.views.sum;
-      }
-    } else {
-      result[url][eIdx].inferredStartDate = new Date(uev.metrics.experimenttime.min)
-        .toISOString();
-      result[url][eIdx].inferredEndDate = new Date(uev.metrics.experimenttime.max)
-        .toISOString();
-    }
+    result[url].push({
+      experiment,
+      variants: [],
+      inferredStartDate: new Date(uev.metrics.experimenttime.min).toISOString(),
+      inferredEndDate: new Date(uev.metrics.experimenttime.max).toISOString(),
+    });
+  });
+  variants.forEach((uev) => {
+    const [url, experiment, variant] = uev.value.split('\u3343');
+    const eIdx = result[url].findIndex((e) => e.experiment === experiment);
+    result[url][eIdx].variants.push({
+      name: variant,
+      click: {},
+      formsubmit: {},
+      convert: {},
+      interactionsCount: uev.metrics.interaction.sum,
+      samples: uev.metrics.views.count,
+      views: uev.metrics.views.sum,
+    });
+  });
+  checkpoints.forEach((uev) => {
+    const [url, experiment, variant, checkpoint, source] = uev.value.split('\u3343');
+    const eIdx = result[url].findIndex((e) => e.experiment === experiment);
+    const vIdx = result[url][eIdx].variants.findIndex((v) => v.name === variant);
+
+    result[url][eIdx].variants[vIdx][checkpoint][source] = {
+      value: uev.metrics.views.sum,
+      samples: uev.metrics.views.count,
+    };
   });
 
   return result;

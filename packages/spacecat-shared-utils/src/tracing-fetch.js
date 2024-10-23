6579a26d917db0bb8dc3baa2e19a0dfee8db5fc8
Dominique Jäggi <1872195+solaris007@users.noreply.github.com>
2024-10-23 16:07:33 +0200

    feat: tracing fetch (#409)
    Add a wrapped fetch with minimal tracing support. Unfortunately we can't
    use the `AWSXRay` SDKs global instrumentation due to:
    https://github.com/aws/aws-xray-sdk-node/issues/487 otherwise documented
    at
    https://docs.aws.amazon.com/xray/latest/devguide/xray-sdk-nodejs-httpclients.html

---
+++ packages/spacecat-shared-utils/src/tracing-fetch.js
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2024 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+import AWSXRay from 'aws-xray-sdk';
+
+import { fetch as adobeFetch } from './adobe-fetch.js';
+
+export async function tracingFetch(url, options = {}) {
+  const parentSegment = AWSXRay.getSegment();
+
+  if (!parentSegment) {
+    return adobeFetch(url, options);
+  }
+
+  const method = options.method || 'GET';
+  const subsegment = parentSegment.addNewSubsegment(`HTTP ${method} ${url}`);
+
+  try {
+    subsegment.addAnnotation('url', url);
+    subsegment.addAnnotation('method', method);
+
+    const response = await adobeFetch(url, options);
+
+    subsegment.addMetadata('statusCode', response.status);
+
+    subsegment.close();
+
+    return response;
+  } catch (error) {
+    subsegment.addError(error);
+    subsegment.close();
+
+    throw error;
+  }
+}

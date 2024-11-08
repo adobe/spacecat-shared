d91f203a1c337b6b6981786f8ec7f225da959ed8
Sweta Barman <swbarman@adobe.com>
2024-11-08 05:11:23 -0500

    Update packages/spacecat-shared-data-access/src/models/api-key/api-key-constants.js
    Co-authored-by: Bruce Lefebvre <blefebvr@adobe.com>

--- packages/spacecat-shared-data-access/src/models/api-key/api-key-constants.js
+++ packages/spacecat-shared-data-access/src/models/api-key/api-key-constants.js
@@ -11,7 +11,7 @@
  */
 
 /**
- * Api Key Status Types
+ * API Key Status Types
  * Any changes to this object needs to be reflected in the index.d.ts file as well.
  */
 export const ApiKeyStatus = {

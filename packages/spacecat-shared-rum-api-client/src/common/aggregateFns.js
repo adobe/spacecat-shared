f4248f1c6bdf6b5a60114f52720f6050cf5ebb7f
ekremney <ekdogan@adobe.com>
2024-11-21 10:34:53 +0100

    fix: remove unnecessary functions

--- packages/spacecat-shared-rum-api-client/src/common/aggregateFns.js
+++ packages/spacecat-shared-rum-api-client/src/common/aggregateFns.js
@@ -13,48 +13,6 @@
 import { extractTrafficHints, classifyVendor, getSecondLevelDomain } from './traffic.js';
 
 /**
- * Calculates the total page views by URL from an array of bundles.
- * @param {Array<Object>} bundles - An array of RUM bundles (NOT Flat bundles).
- * @returns {Object} An object where keys are URLs and values are the total page views for each URL.
- */
-function pageviewsByUrl(bundles) {
-  return bundles.reduce((acc, cur) => {
-    if (!acc[cur.url]) acc[cur.url] = 0;
-    acc[cur.url] += cur.weight;
-    return acc;
-  }, {});
-}
-
-/**
- * Calculates the Click-Through Rate (CTR) by URL.
- * CTR is defined as the total number of sessions with at least one click event
- * divided by the total number of pageviews for each URL.
- *
- * @param {Array<Object>} bundles - An array of RUM bundles (NOT Flat bundles).
- * @returns {Object} - An object where the key is the URL and the value is the CTR value.
- */
-function getCTRByUrl(bundles) {
-  const aggregated = bundles.reduce((acc, bundle) => {
-    const { url } = bundle;
-    if (!acc[url]) {
-      acc[url] = { sessionsWithClick: 0, totalPageviews: 0 };
-    }
-    const hasClick = bundle.events.some((event) => event.checkpoint === 'click');
-
-    acc[url].totalPageviews += bundle.weight;
-    if (hasClick) {
-      acc[url].sessionsWithClick += bundle.weight;
-    }
-    return acc;
-  }, {});
-  return Object.entries(aggregated)
-    .reduce((acc, [url, { sessionsWithClick, totalPageviews }]) => {
-      acc[url] = (sessionsWithClick / totalPageviews);
-      return acc;
-    }, {});
-}
-
-/**
  * Calculates the Click-Through Rate (CTR) by URL and Referrer.
  * CTR is defined as the total number of sessions with at least one click event per referrer.
  * divided by the total number of pageviews for each URL per referrer.

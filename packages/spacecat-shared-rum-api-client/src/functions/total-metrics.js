99a7c66f18ff1ba869ffdb07b17b9505d1f00069
Alina Rublea <rublea@adobe.com>
2024-12-20 11:24:33 +0100

    feat: support site metrics overview
    …mport workers
    
    Please ensure your pull request adheres to the following guidelines:
    - [ ] make sure to link the related issues in this description
    - [ ] when merging / squashing, make sure the fixed issue references are
    visible in the commits, for easy compilation of release notes
    
    ## Related Issues
    
    
    Thanks for contributing!
    
    ---------
    
    Co-authored-by: Ekrem Doğan <ekremney@users.noreply.github.com>

---
+++ packages/spacecat-shared-rum-api-client/src/functions/total-metrics.js
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2024 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+import { DataChunks, series } from '@adobe/rum-distiller';
+import { loadBundles } from '../utils.js';
+
+function handler(bundles) {
+  const dataChunks = new DataChunks();
+  loadBundles(bundles, dataChunks);
+  dataChunks.addSeries('traffic_domain', series.pageViews);
+  dataChunks.addSeries('ctr', (bundle) => (bundle.events.some((e) => e.checkpoint === 'click')
+    ? bundle.weight
+    : 0));
+  const totalPageViews = dataChunks?.totals?.traffic_domain?.weight;
+  const sum = dataChunks?.totals?.ctr?.sum ?? 0;
+  const weight = dataChunks?.totals?.ctr?.weight ?? 0;
+  const totalCTR = weight !== 0 ? sum / weight : 0;
+  return {
+    totalPageViews,
+    totalCTR,
+  };
+}
+
+export default {
+  handler,
+  checkpoints: ['click'],
+};

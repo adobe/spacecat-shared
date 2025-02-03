7d95872a7e9c1a5ec6b0413ccca4b1d901dfdad4
Alina Rublea <rublea@adobe.com>
2025-02-03 10:39:07 +0100

    fix: use Mozilla user agent
    Please ensure your pull request adheres to the following guidelines:
    - [ ] make sure to link the related issues in this description
    - [ ] when merging / squashing, make sure the fixed issue references are
    visible in the commits, for easy compilation of release notes
    
    ## Related Issues
    
    
    Thanks for contributing!

--- packages/spacecat-shared-utils/src/url-helpers.js
+++ packages/spacecat-shared-utils/src/url-helpers.js
@@ -88,7 +88,7 @@ async function composeAuditURL(url) {
   const resp = await fetch(urlWithScheme, {
     method: 'GET',
     headers: {
-      'User-Agent': 'curl/7.88.1',
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
     },
   });
   const finalUrl = resp.url.split('://')[1];

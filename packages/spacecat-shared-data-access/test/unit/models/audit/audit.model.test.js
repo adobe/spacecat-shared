46aa15daf89f92934f71d20e228172d98dad3e69
martinst06 <mstoyanov@adobe.com>
2025-02-06 13:53:54 +0100

    test: refactored audit types test

--- packages/spacecat-shared-data-access/test/unit/models/audit/audit.model.test.js
+++ packages/spacecat-shared-data-access/test/unit/models/audit/audit.model.test.js
@@ -149,15 +149,32 @@ describe('AuditModel', () => {
   });
 
   describe('AuditTypes', () => {
+    const auditTypes = Object.values(Audit.AUDIT_TYPES);
+    const expectedAuditTypes = [
+      '404',
+      'broken-backlinks',
+      'experimentation',
+      'organic-keywords',
+      'organic-traffic',
+      'cwv',
+      'lhs-desktop',
+      'lhs-mobile',
+      'experimentation-ess-monthly',
+      'experimentation-ess-daily',
+      'high-organic-low-ctr',
+      'broken-internal-links',
+      'meta-tags',
+      'sitemap',
+      'structured-data',
+    ];
+
     it('should have all audit types present in AUDIT_TYPES', () => {
-      const expectedAuditTypes = ['404', 'broken-backlinks', 'experimentation', 'organic-keywords', 'organic-traffic', 'cwv', 'lhs-desktop', 'lhs-mobile', 'experimentation-ess-monthly', 'experimentation-ess-daily', 'high-organic-low-ctr', 'broken-internal-links', 'meta-tags', 'sitemap', 'structured-data'];
-      const auditTypes = Object.values(Audit.AUDIT_TYPES);
       expect(auditTypes).to.have.members(expectedAuditTypes);
     });
 
     it('no unexpected audit types are present in AUDIT_TYPES', () => {
       const unexpectedAuditTypes = ['unexpected1', 'unexpected2'];
-      const auditTypes = Object.values(Audit.AUDIT_TYPES);
+      expect(auditTypes).to.have.members(expectedAuditTypes);
       expect(auditTypes).to.not.have.members(unexpectedAuditTypes);
     });
   });

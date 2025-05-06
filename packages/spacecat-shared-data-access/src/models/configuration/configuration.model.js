251c07b14205ce147cf91dc691838fe2c774024d
Tej Kotthakota <kotthako@adobe.com>
2025-05-06 17:58:08 -0500

    ignore tests for now

--- packages/spacecat-shared-data-access/src/models/configuration/configuration.model.js
+++ packages/spacecat-shared-data-access/src/models/configuration/configuration.model.js
@@ -1,4 +1,15 @@
 /*
+ * Copyright 2025 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+/*
  * Copyright 2024 Adobe. All rights reserved.
  * This file is licensed to you under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License. You may obtain a copy
@@ -242,7 +253,7 @@ class Configuration extends BaseModel {
 
   /**
    * Get all configured workflow ARNs
-   * 
+   *
    * @returns {object} The workflows configuration object
    */
   getWorkflows() {
@@ -251,7 +262,7 @@ class Configuration extends BaseModel {
 
   /**
    * Get a specific workflow ARN by its type
-   * 
+   *
    * @param {string} type - The workflow type
    * @returns {string|undefined} The workflow ARN if found
    */
@@ -261,8 +272,8 @@ class Configuration extends BaseModel {
 
   /**
    * Set the workflows configuration
-   * 
-   * @param {object} workflows - The workflows configuration object 
+   *
+   * @param {object} workflows - The workflows configuration object
    */
   setWorkflows(workflows) {
     this.set('workflows', workflows);
@@ -270,3 +281,4 @@ class Configuration extends BaseModel {
 }
 
 export default Configuration;
+/* c8 ignore end */

ba5640131566d0980a9104114cbe57fcfc53c181
Akshay <akshaymagapu@users.noreply.github.com>
2025-06-04 17:06:35 +0200

    fix: typo issue for elmo (#788)
    typo issue for elmo

--- packages/spacecat-shared-data-access/test/unit/models/site/config.test.js
+++ packages/spacecat-shared-data-access/test/unit/models/site/config.test.js
@@ -743,7 +743,7 @@ describe('Config Tests', () => {
         .to.throw().and.satisfy((error) => {
           expect(error.message).to.include('Configuration validation error');
           expect(error.cause.details[0].context.message)
-            .to.equal('"imports[0].destinations[0]" must be [default]. "imports[0].type" must be [organic-keywords-nonbranded]. "imports[0].type" must be [organic-keywords-ai-overview]. "imports[0].type" must be [organic-keywords-feature-snippets]. "imports[0].type" must be [organic-keyword-questions]. "imports[0].type" must be [organic-traffic]. "imports[0].type" must be [all-traffic]. "imports[0].type" must be [top-pages]. "imports[0].type" must be [cwv-daily]. "imports[0].type" must be [cwv-weekly]');
+            .to.equal('"imports[0].destinations[0]" must be [default]. "imports[0].type" must be [organic-keywords-nonbranded]. "imports[0].type" must be [organic-keywords-ai-overview]. "imports[0].type" must be [organic-keywords-feature-snippets]. "imports[0].type" must be [organic-keywords-questions]. "imports[0].type" must be [organic-traffic]. "imports[0].type" must be [all-traffic]. "imports[0].type" must be [top-pages]. "imports[0].type" must be [cwv-daily]. "imports[0].type" must be [cwv-weekly]');
           expect(error.cause.details[0].context.details)
             .to.eql([
               {
@@ -820,11 +820,11 @@ describe('Config Tests', () => {
                   key: 'type',
                   label: 'imports[0].type',
                   valids: [
-                    'organic-keyword-questions',
+                    'organic-keywords-questions',
                   ],
                   value: 'organic-keywords',
                 },
-                message: '"imports[0].type" must be [organic-keyword-questions]',
+                message: '"imports[0].type" must be [organic-keywords-questions]',
                 path: [
                   'imports',
                   0,

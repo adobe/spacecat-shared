d188ec8e61386372cedfc2bd6a08cd05a8302971
Daniel Huser <djc@adobe.com>
2025-06-04 10:05:51 +0200

    Added IT

---
+++ packages/spacecat-shared-data-access/test/fixtures/scrape-jobs.fixture.js
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2024 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+import { ScrapeJob } from '../../src/index.js';
+
+const scrapeJobs = [
+  {
+    scrapeJobId: '021cbb7d-0772-45c6-967c-86a0a598b7dd',
+    scrapeQueueId: 'Q-123',
+    hashedApiKey: 'some-key-1',
+    baseURL: 'https://example-1.com/cars',
+    startedAt: '2023-12-06T08:17:41.467Z',
+    status: ScrapeJob.ScrapeJobStatus.RUNNING,
+    initiatedBy: {
+      apiKeyName: 'K-123',
+    },
+    options: {
+      [ScrapeJob.ScrapeOptions.ENABLE_JAVASCRIPT]: true,
+      [ScrapeJob.ScrapeOptions.PAGE_LOAD_TIMEOUT]: 10000,
+      [ScrapeJob.ScrapeOptions.HIDE_CONSENT_BANNER]: true,
+      [ScrapeJob.ScrapeOptions.WAIT_FOR_SELECTOR]: 'body',
+      [ScrapeJob.ScrapeOptions.SCREENSHOT_TYPES]: [
+        ScrapeJob.ScrapeScreenshotType.SCROLL,
+        ScrapeJob.ScrapeScreenshotType.BLOCK,
+      ],
+    },
+    customHeaders: {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    },
+  },
+  {
+    scrapeJobId: '72113a4d-ca45-4c35-bd2e-29bb0ec03435',
+    scrapeQueueId: 'Q-321',
+    hashedApiKey: 'some-key-1',
+    baseURL: 'https://example-2.com/cars',
+    startedAt: '2023-11-15T01:22:05.000Z',
+    status: ScrapeJob.ScrapeJobStatus.FAILED,
+    initiatedBy: {
+      apiKeyName: 'K-321',
+    },
+    options: {
+      [ScrapeJob.ScrapeOptions.ENABLE_JAVASCRIPT]: false,
+      [ScrapeJob.ScrapeOptions.HIDE_CONSENT_BANNER]: false,
+      [ScrapeJob.ScrapeOptions.WAIT_FOR_SELECTOR]: 'body',
+      [ScrapeJob.ScrapeOptions.SCREENSHOT_TYPES]: [
+        ScrapeJob.ScrapeScreenshotType.SCROLL,
+        ScrapeJob.ScrapeScreenshotType.BLOCK,
+      ],
+    },
+  },
+  {
+    importJobId: '78e1f8de-661a-418b-bd80-24589a10b5ce',
+    importQueueId: 'Q-213',
+    hashedApiKey: 'some-key-2',
+    baseURL: 'https://example-3.com/',
+    startedAt: '2023-11-15T03:46:40.000Z',
+    endedAt: '2023-11-15T03:49:13.000Z',
+    status: ScrapeJob.ScrapeJobStatus.COMPLETE,
+    initiatedBy: {
+      apiKeyName: 'K-322',
+    },
+    options: {
+      [ScrapeJob.ScrapeOptions.ENABLE_JAVASCRIPT]: false,
+    },
+    hasCustomImportJs: false,
+    hasCustomHeaders: true,
+  },
+];
+
+export default scrapeJobs;

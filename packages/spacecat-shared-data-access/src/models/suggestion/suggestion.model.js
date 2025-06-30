1dca2a5ae322080d2d2fc0e54e4142a09e73bdd9
Daniel Huser <djc@adobe.com>
2025-06-30 07:42:15 +0200

    added suggestion type for paid traffic

--- packages/spacecat-shared-data-access/src/models/suggestion/suggestion.model.js
+++ packages/spacecat-shared-data-access/src/models/suggestion/suggestion.model.js
@@ -36,6 +36,7 @@ class Suggestion extends BaseModel {
     CONTENT_UPDATE: 'CONTENT_UPDATE',
     REDIRECT_UPDATE: 'REDIRECT_UPDATE',
     METADATA_UPDATE: 'METADATA_UPDATE',
+    AI_INSIGHT: 'AI_INSIGHT',
   };
 
   // add your customized methods here

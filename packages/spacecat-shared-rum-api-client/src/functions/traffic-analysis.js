11d207f60b316be50360b8f914e301520249e909
Christopher Wisse <cwisse@adobe.com>
2025-07-08 11:55:21 +0200

    feat(shared-utils): add S3 storage functionality for paid traffic analysis import

--- packages/spacecat-shared-rum-api-client/src/functions/traffic-analysis.js
+++ packages/spacecat-shared-rum-api-client/src/functions/traffic-analysis.js
@@ -10,112 +10,42 @@
  * governing permissions and limitations under the License.
  */
 
-import { classifyTraffic } from '../common/traffic.js';
+import { classifyTrafficSource, extractTrafficHints } from '../common/traffic.js';
 
-function getTrafficType(bundle, memo) {
+function getTrafficType(bundle, memo, referrer, utmSource, utmMedium, tracking) {
   const key = `${bundle.id}${bundle.url}${bundle.time}`;
   if (memo[key]) return memo[key];
 
-  const type = classifyTraffic(bundle);
+  const type = classifyTrafficSource(
+    bundle.url,
+    referrer,
+    utmSource,
+    utmMedium,
+    tracking,
+  );
+
   // eslint-disable-next-line no-param-reassign
   memo[key] = type;
   return type;
-}
-
-const checkpoints = {
-  /* eslint-disable no-param-reassign */
-  utm: (result, source, target) => {
-    if (source === 'utm_source') result.utmSource = target;
-    else if (source === 'utm_medium') result.utmMedium = target;
-    else if (source === 'utm_campaign') result.utmCampaign = target;
-  },
-  consent: (result, target) => {
-    result.consent = target;
-  },
-  404: (result, source) => {
-    result.notFoundSource = source;
-  },
-  click: (result) => {
-    result.hasClick = true;
-  },
-  enter: (result) => {
-    result.hasEntry = true;
-  },
-  viewmedia: (result, timeDelta) => {
-    if (timeDelta >= 10000) {
-      result.hasScroll = true;
-    }
-  },
-  viewblock: (result, timeDelta) => {
-    if (timeDelta >= 10000) {
-      result.hasScroll = true;
-    }
-  },
-  'cwv-lcp': (result, value) => {
-    if (value > (result.lcp || 0)) result.lcp = value;
-  },
-  'cwv-cls': (result, value) => {
-    if (value > (result.cls || 0)) result.cls = value;
-  },
-  'cwv-inp': (result, value) => {
-    if (value > (result.inp || 0)) result.inp = value;
-  },
-  /* eslint-enable no-param-reassign */
-};
-
-function extractEventData(bundle) {
-  const result = {
-    utmSource: undefined,
-    utmMedium: undefined,
-    utmCampaign: undefined,
-    consent: undefined,
-    notFoundSource: undefined,
-    hasClick: false,
-    hasScroll: false,
-    hasEntry: false,
-    lcp: undefined,
-    cls: undefined,
-    inp: undefined,
-  };
-
-  for (const {
-    checkpoint, source, target, timeDelta, value,
-  } of bundle.events) {
-    if (checkpoints[checkpoint]) {
-      checkpoints[checkpoint](result, source, target, timeDelta, value);
-    }
-  }
-
-  return result;
 }
 
 async function handler(bundles) {
   const memo = {};
 
   const result = bundles.map((bundle) => {
-    const trafficType = getTrafficType(bundle, memo);
-    const eventData = extractEventData(bundle);
-    const hasEngagedClick = eventData.hasClick && eventData.consent !== 'show';
+    const {
+      referrer, utmSource, utmMedium, tracking,
+    } = extractTrafficHints(bundle);
+
+    const trafficType = getTrafficType(bundle, memo, referrer, utmSource, utmMedium, tracking);
 
     return {
-      path: new URL(bundle.url).pathname,
-      type: trafficType.type,
-      channel: trafficType.category,
-      platform: trafficType.vendor,
-      device: bundle.userAgent.split(':')[0],
-      source: eventData.utmSource,
-      medium: eventData.utmMedium,
-      campaign: eventData.utmCampaign,
-      referrer: bundle.referrer,
-      consent: eventData.consent,
-      notfound: eventData.notFoundSource,
-      pageviews: bundle.weight,
-      clicked: hasEngagedClick ? 1 : 0,
-      entry: eventData.hasEntry ? 1 : 0,
-      engaged: (hasEngagedClick || eventData.hasScroll) ? 1 : 0,
-      lcp: eventData.lcp,
-      cls: eventData.cls,
-      inp: eventData.inp,
+      ...bundle,
+      ...trafficType,
+      referrer,
+      utmSource,
+      utmMedium,
+      tracking,
     };
   });
 

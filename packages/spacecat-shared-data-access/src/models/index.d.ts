1a70b87b257a6e0f5794d633cdd832f7128d837a
dmaurya929 <dmaurya929@gmail.com>
2025-07-23 00:07:38 +0530

    feat: add top form model

--- packages/spacecat-shared-data-access/src/models/index.d.ts
+++ packages/spacecat-shared-data-access/src/models/index.d.ts
@@ -26,5 +26,6 @@ export type * from './scrape-job';
 export type * from './scrape-url';
 export type * from './site';
 export type * from './site-candidate';
+export type * from './site-top-form';
 export type * from './site-top-page';
 export type * from './suggestion';

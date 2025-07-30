07a92ce2659e27a85058d1d40942ec7e8b8897e3
David Aurelio <158859+davidaurelio@users.noreply.github.com>
2025-07-30 17:58:56 +0200

    feat: Add `'never'` to job intervals (#885) (#886)
    Add `'never'` as a valid value for job intervals.
    
    This enables activating imports for sites without ever having them
    scheduled by `spacecat-job-dispatcher`.
    
    That's useful for imports that are invoked manually via the `@spacecat`
    bot or triggered by audits.
    
    For ahrefs-based imports specifically, this is meant to drive down the
    consumption of Ahrefs API Units.
    
    ## Related Issues
    
    https://jira.corp.adobe.com/browse/LLMO-186
    
    Please ensure your pull request adheres to the following guidelines:
    - [x] make sure to link the related issues in this description
    - [x] when merging / squashing, make sure the fixed issue references are
    visible in the commits, for easy compilation of release notes

--- packages/spacecat-shared-data-access/src/models/configuration/configuration.model.js
+++ packages/spacecat-shared-data-access/src/models/configuration/configuration.model.js
@@ -31,7 +31,7 @@ class Configuration extends BaseModel {
   };
 
   static JOB_INTERVALS = {
-    NEVER: 'never', // allows to enable imports without scheduling them
+    NEVER: 'never', // allows to enable imports without scheduling them.
     EVERY_HOUR: 'every-hour',
     DAILY: 'daily',
     WEEKLY: 'weekly',

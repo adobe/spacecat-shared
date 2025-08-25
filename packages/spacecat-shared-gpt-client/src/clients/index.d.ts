3cc60fed9c6b68743f5bf15e6ce536fbf57ba061
anagarwa <anagarwa@adobe.com>
2025-08-25 15:52:01 +0530

    fix: adding azure openai api client

--- packages/spacecat-shared-gpt-client/src/clients/index.d.ts
+++ packages/spacecat-shared-gpt-client/src/clients/index.d.ts
@@ -12,111 +12,50 @@
 
 import type { UniversalContext } from '@adobe/helix-universal';
 
-export class FirefallClient {
+export class AzureOpenAIClient {
   /**
-   * Creates a new FirefallClient instance from the given UniversalContext.
-   * @param {UniversalContext} context The UniversalContext to use for creating the FirefallClient.
-   * @returns {FirefallClient} The FirefallClient instance.
+   * Creates a new AzureOpenAIClient instance from the given UniversalContext.
+   * @param {UniversalContext} context The UniversalContext to use for creating the AzureOpenAIClient.
+   * @returns {AzureOpenAIClient} The AzureOpenAIClient instance.
    */
-  static createFrom(context: UniversalContext): FirefallClient;
+  static createFrom(context: UniversalContext): AzureOpenAIClient;
 
   /**
-   * Sends the given prompt to the Firefall GPT API and returns the response.
-   * @param {string} prompt The prompt to send to the Firefall GPT API.
-   * @returns {Promise<string>} The response from the Firefall GPT API.
-   * @deprecated since version 1.2.19. Use fetchCapabilityExecution instead.
-   */
-  fetch(prompt: string): Promise<string>;
-
-  /**
-   * Fetches data from Firefall Chat Completion API.
+   * Fetches data from Azure OpenAI Chat Completion API.
    *
-   * @param {string} prompt - The text prompt to provide to Firefall
+   * @param {string} prompt - The text prompt to provide to Azure OpenAI
    * @param {object} [options] - The options for the call, with optional properties:
-   * - imageUrls: An array of URLs of the images to provide to Firefall
-   * - model: LLM Model to use (default: gpt-4-turbo).
-   * Use 'gpt-4-vision' with images.
-   * JSON mode is only currently supported with the following models: gpt-35-turbo-1106, gpt-4-turbo
+   * - imageUrls: An array of URLs of the images to provide to Azure OpenAI
+   * - responseFormat: The response format to request from Azure OpenAI (accepts: json_object)
    * @returns {Promise<object>} A promise that resolves to an object containing the chat completion.
    *
    * The returned object has the following structure:
    *
    * @example
    * {
-   *   "conversation_identifier": string | null,
-   *   "query_id": string | null,
+   *   "id": string,
+   *   "object": string,
+   *   "created": number,
    *   "model": string,
    *   "choices": [
    *     {
-   *       "finish_reason": string,
    *       "index": number,
    *       "message": {
    *         "role": string,
-   *         "content": string,
-   *         "function_call": object | null
+   *         "content": string
    *       },
-   *       "content_filter_results": {
-   *         "hate": {
-   *           "filtered": boolean,
-   *           "severity": string
-   *         },
-   *         "self_harm": {
-   *           "filtered": boolean,
-   *           "severity": string
-   *         },
-   *         "sexual": {
-   *           "filtered": boolean,
-   *           "severity": string
-   *         },
-   *         "violence": {
-   *           "filtered": boolean,
-   *           "severity": string
-   *         }
-   *       },
-   *       "logprobs": object | null
+   *       "finish_reason": string
    *     }
    *   ],
-   *   "created_at": string,
    *   "usage": {
-   *     "completion_tokens": number,
    *     "prompt_tokens": number,
+   *     "completion_tokens": number,
    *     "total_tokens": number
-   *   },
-   *   "prompt_filter_results": [
-   *     {
-   *       "prompt_index": number,
-   *       "content_filter_results": {
-   *         "hate": {
-   *           "filtered": boolean,
-   *           "severity": string
-   *         },
-   *         "jailbreak": {
-   *           "filtered": boolean,
-   *           "detected": boolean
-   *         },
-   *         "self_harm": {
-   *           "filtered": boolean,
-   *           "severity": string
-   *         },
-   *         "sexual": {
-   *           "filtered": boolean,
-   *           "severity": string
-   *         },
-   *         "violence": {
-   *           "filtered": boolean,
-   *           "severity": string
-   *         }
-   *       }
-   *     }
-   *   ]
+   *   }
    * }
    */
-  fetchChatCompletion(prompt: string, options?: object): Promise<object>;
-
-  /**
-   * Fetches data from Firefall API.
-   * @param prompt The text prompt to provide to Firefall
-   * @returns {Promise<string>} - AI response
-   */
-  fetchCapabilityExecution(prompt: string): Promise<string>;
+  fetchChatCompletion(prompt: string, options?: {
+    imageUrls?: string[];
+    responseFormat?: string;
+  }): Promise<object>;
 }

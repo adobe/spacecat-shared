589506dfa2b4abdf861b9741402b2b9f9020f82b
Divyansh Pratap <dipratap@adobe.com>
2025-09-30 03:07:08 +0530

    fix: add user-engagement import type

--- packages/spacecat-shared-data-access/src/models/site/config.js
+++ packages/spacecat-shared-data-access/src/models/site/config.js
@@ -27,6 +27,7 @@ export const IMPORT_TYPES = {
   CWV_WEEKLY: 'cwv-weekly',
   TRAFFIC_ANALYSIS: 'traffic-analysis',
   TOP_FORMS: 'top-forms',
+  USER_ENGAGEMENT: 'user-engagement',
 };
 
 export const IMPORT_DESTINATIONS = {

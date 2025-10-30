12b7c0a3d08561ce55dbdbd7c6fedb0fd9154e9b
Ravi Kiran Papani <rpapani@adobe.com>
2025-10-29 20:47:30 -0700

    fix: add failure status text to updatePageMetadata

--- packages/spacecat-shared-content-client/src/clients/content-client.js
+++ packages/spacecat-shared-content-client/src/clients/content-client.js
@@ -402,9 +402,7 @@ export default class ContentClient {
 
     const response = await document.updateMetadata(mergedMetadata);
     if (response?.status !== 200) {
-      this.log.debug(`Failed to update metadata for path ${path}: ${response.statusText}`);
-      this.log.debug(`Response: ${JSON.stringify(response)}`);
-      throw new Error(`Failed to update metadata for path ${path}`);
+      throw new Error(`Failed to update metadata for path ${path}: ${response.statusText}`);
     }
 
     this.#logDuration('updatePageMetadata', startTime);

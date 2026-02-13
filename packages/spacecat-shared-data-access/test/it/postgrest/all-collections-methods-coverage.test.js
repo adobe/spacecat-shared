d3c0f0d013f8c7ac92e883001ec7bea5c874cdc7
ekremney <ekdogan@adobe.com>
2026-02-13 17:16:13 +0100

    test(data-access): expand postgrest integration coverage for all collections

---
+++ packages/spacecat-shared-data-access/test/it/postgrest/all-collections-methods-coverage.test.js
@@ -0,0 +1,180 @@
+/*
+ * Copyright 2026 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+import { expect } from 'chai';
+
+import { createITDataAccess, TEST_IDS, TEST_VALUES } from './helpers.js';
+
+const GENERIC_UUID = '11111111-1111-4111-8111-111111111111';
+const GENERIC_URL = 'https://it.example.com';
+
+const getMethodNames = (collection) => {
+  const names = new Set();
+
+  let proto = collection;
+  while (proto && proto !== Object.prototype) {
+    Object.getOwnPropertyNames(proto).forEach((name) => {
+      if (name === 'constructor' || name.startsWith('_')) {
+        return;
+      }
+
+      if (typeof collection[name] === 'function') {
+        names.add(name);
+      }
+    });
+
+    proto = Object.getPrototypeOf(proto);
+  }
+
+  Object.getOwnPropertyNames(collection).forEach((name) => {
+    if (name.startsWith('_')) {
+      return;
+    }
+
+    if (typeof collection[name] === 'function') {
+      names.add(name);
+    }
+  });
+
+  return [...names]
+    .filter((name) => name !== 'clazz')
+    .sort();
+};
+
+const KEY_EVENT_DEPRECATED_METHODS = new Set([
+  'all',
+  'allByIndexKeys',
+  'findById',
+  'findByIndexKeys',
+  'create',
+  'createMany',
+  'removeByIds',
+  'removeByIndexKeys',
+]);
+
+const getArgsForMethod = (entityName, methodName) => {
+  switch (methodName) {
+    case 'all':
+      return [{}, { limit: 1 }];
+    case 'allByIndexKeys':
+      return [{}, { limit: 1 }];
+    case 'findByAll':
+      return [{}, { limit: 1 }];
+    case 'findByIndexKeys':
+      return [{}, { limit: 1 }];
+    case 'findById': {
+      if (entityName === 'LatestAudit') {
+        return [TEST_IDS.siteId, '404'];
+      }
+      if (entityName === 'AuditUrl') {
+        return [TEST_IDS.siteId, GENERIC_URL];
+      }
+      if (entityName === 'SentimentGuideline' || entityName === 'SentimentTopic') {
+        return [TEST_IDS.siteId, GENERIC_UUID];
+      }
+      return [GENERIC_UUID];
+    }
+    case 'existsById':
+      return [GENERIC_UUID];
+    case 'batchGetByKeys':
+      return [[{ id: GENERIC_UUID }]];
+    case 'create':
+      return [{}];
+    case 'createMany':
+      return [[{}]];
+    case 'updateByKeys':
+      return [{ id: GENERIC_UUID }, {}];
+    case 'removeByIds':
+      return [[GENERIC_UUID]];
+    case 'removeByIndexKeys':
+      return [[{ id: GENERIC_UUID }]];
+    case 'allByDateRange':
+      return ['2025-01-01', '2025-02-01'];
+    case 'allRecentByUrlAndProcessingType':
+      return [GENERIC_URL, 'default', 24];
+    case 'findByPreviewURL':
+      return [TEST_VALUES.csPreviewURL];
+    case 'allWithLatestAudit':
+      return ['404', 'desc'];
+    case 'allByProjectName':
+      return [TEST_VALUES.projectName];
+    case 'allByOrganizationIdAndProjectId':
+      return [TEST_IDS.organizationId, TEST_IDS.projectId];
+    case 'allByOrganizationIdAndProjectName':
+      return [TEST_IDS.organizationId, TEST_VALUES.projectName];
+    case 'allByAuditType':
+      return ['404'];
+    case 'findBySiteIdAndUrl':
+      return [TEST_IDS.siteId, GENERIC_URL];
+    case 'allBySiteIdAndAuditType':
+      return [TEST_IDS.siteId, '404', { limit: 1 }];
+    case 'allBySiteIdSorted':
+      return [TEST_IDS.siteId, { limit: 1 }];
+    case 'allBySiteIdByCustomerSorted':
+      return [TEST_IDS.siteId, true, { limit: 1 }];
+    case 'removeForSiteId':
+      return [TEST_IDS.siteId];
+    case 'removeForSiteIdByCustomer':
+      return [TEST_IDS.siteId, true];
+    case 'removeByUrlAndFormSource':
+      return [GENERIC_URL, ''];
+    case 'findByUrlAndFormSource':
+      return [GENERIC_URL, ''];
+    case 'getSuggestionsByFixEntityId':
+      return [GENERIC_UUID];
+    case 'setSuggestionsForFixEntity':
+      return [GENERIC_UUID, null, []];
+    case 'getAllFixesWithSuggestionByCreatedAt':
+      return [GENERIC_UUID, '2025-01-01'];
+    case 'bulkUpdateStatus':
+      return [[], 'NEW'];
+    case 'getFixEntitiesBySuggestionId':
+      return [GENERIC_UUID];
+    case 'findByVersion':
+      return ['v1'];
+    default:
+      return [];
+  }
+};
+
+describe('PostgREST IT - all collections methods coverage', () => {
+  const dataAccess = createITDataAccess();
+
+  const entityNames = Object.keys(dataAccess).sort();
+
+  entityNames.forEach((entityName) => {
+    const collection = dataAccess[entityName];
+    const methodNames = getMethodNames(collection);
+
+    describe(`${entityName} collection`, () => {
+      methodNames.forEach((methodName) => {
+        it(`invokes ${entityName}.${methodName}()`, async () => {
+          const method = collection[methodName];
+          expect(method).to.be.a('function');
+
+          const args = getArgsForMethod(entityName, methodName);
+
+          try {
+            await method.apply(collection, args);
+          } catch (error) {
+            expect(error).to.be.instanceOf(Error);
+
+            // Ensure v3 deprecation remains explicit.
+            if (entityName === 'KeyEvent' && KEY_EVENT_DEPRECATED_METHODS.has(methodName)) {
+              expect(error.message).to.include('KeyEvent is deprecated in data-access v3');
+            }
+          }
+        });
+      });
+    });
+  });
+});
---
+++ packages/spacecat-shared-data-access/test/it/postgrest/helpers.js
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2026 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+import { createDataAccess } from '../../../src/service/index.js';
+
+export const TEST_IDS = {
+  organizationId: '04f63783-3f76-4076-bbda-71a11145303c',
+  projectId: '6fc3365d-f400-4a17-91e9-7141e5b82350',
+  siteId: '0983c6da-0dee-45cc-b897-3f1fed6b460b',
+  siteIdSecondary: 'e12c091c-075b-4c94-aab7-398a04412b5c',
+  auditId: '74a041ed-a15c-4bdc-a38c-5203b22d2ab3',
+};
+
+export const TEST_VALUES = {
+  siteBaseURL: 'https://tenant-alphaland.com',
+  projectName: 'tenant-alphaland.com',
+  csPreviewURL: 'https://author-p50513-e440257.adobeaemcloud.com',
+};
+
+export const createLogger = () => ({
+  info: () => {},
+  debug: () => {},
+  error: () => {},
+  warn: () => {},
+  trace: () => {},
+});
+
+export const createITDataAccess = () => {
+  const postgrestUrl = process.env.POSTGREST_URL || 'http://127.0.0.1:3300';
+  return createDataAccess({ postgrestUrl }, createLogger());
+};

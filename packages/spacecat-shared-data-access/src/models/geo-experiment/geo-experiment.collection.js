58315cd973c373b399429bf11ff1b958d3cfe9c8
Divyansh Pratap <dipratap@adobe.com>
2026-04-07 17:44:23 +0530

    fix: update geo experiment schema for experimentation engine (#1512)
    Update the corresponding data access classes for updated geo experiment
    table schema. https://github.com/adobe/mysticat-data-service/pull/295

--- packages/spacecat-shared-data-access/src/models/geo-experiment/geo-experiment.collection.js
+++ packages/spacecat-shared-data-access/src/models/geo-experiment/geo-experiment.collection.js
@@ -12,6 +12,7 @@
 
 import { hasText } from '@adobe/spacecat-shared-utils';
 import BaseCollection from '../base/base.collection.js';
+import GeoExperiment from './geo-experiment.model.js';
 
 class GeoExperimentCollection extends BaseCollection {
   static COLLECTION_NAME = 'GeoExperimentCollection';
@@ -37,6 +38,27 @@ class GeoExperimentCollection extends BaseCollection {
       data: result.data || [],
       cursor: result.cursor,
     };
+  }
+
+  /**
+   * Gets all geo experiments that are currently active (GENERATING_BASELINE or IN_PROGRESS).
+   * Used by the experimentation engine to discover which experiments to process on each cron tick.
+   *
+   * @param {object} [options={}] - Query options (limit, cursor, order).
+   * @returns {Promise<GeoExperiment[]>} Array of active experiments.
+   */
+  async allActive(options = {}) {
+    const activeStatuses = [
+      GeoExperiment.STATUSES.GENERATING_BASELINE,
+      GeoExperiment.STATUSES.IN_PROGRESS,
+    ];
+    return this.all(
+      {},
+      {
+        ...options,
+        where: (attrs, op) => op.in(attrs.status, activeStatuses),
+      },
+    );
   }
 }
 

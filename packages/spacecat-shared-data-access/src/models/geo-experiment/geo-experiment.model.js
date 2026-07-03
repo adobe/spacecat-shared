2872ac0fad7ff2afa7557882b0430e59240529a8
Divyansh Pratap <dipratap@adobe.com>
2026-07-03 13:59:00 +0530

    fix: add prompt gen started phase

--- packages/spacecat-shared-data-access/src/models/geo-experiment/geo-experiment.model.js
+++ packages/spacecat-shared-data-access/src/models/geo-experiment/geo-experiment.model.js
@@ -30,6 +30,7 @@ class GeoExperiment extends BaseModel {
 
   static PHASES = {
     INITIATED: 'initiated',
+    PROMPT_GENERATION_STARTED: 'prompt_generation_started',
     PROMPT_GENERATION_COMPLETED: 'prompt_generation_completed',
     PRE_ANALYSIS_STARTED: 'pre_analysis_started',
     PRE_ANALYSIS_DONE: 'pre_analysis_done',
